0f0761fdc0758817064063f445d128a50f1091b5
Papa Diallo <albaa.app@gmail.com>
2024-05-21 14:17:03 +0000

    chore: merged changed from refactor/folders-files-structure

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,12 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { MongoBasicQueriesService } from 'src/commons/services/mongo-basic-queries.service';
-// import { IUser } from './interfaces/user.interface';
-// import { userModelName } from './schemas/user.model-name';
 import { UserDto } from './dto/user.schema';
 
 @Injectable()
-// export class UsersService extends MongoBasicQueriesService<IUser> {
 export class UsersService extends MongoBasicQueriesService<UserDto> {
   constructor(@InjectModel(UserDto.name) private model: Model<UserDto>) {
     super(model);
