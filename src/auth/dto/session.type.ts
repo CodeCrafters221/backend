c8b4def6fcff5a6ed30266ac398afcc0a882a435
Papa Diallo <albaa.app@gmail.com>
2024-05-21 14:19:42 +0000

    fix: session type moved to graphql folder

---
+++ src/auth/dto/session.type.ts
@@ -0,0 +1,15 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { ApiProperty } from '@nestjs/swagger';
+import { IUser } from 'src/users/dto/user.schema';
+import { User } from 'src/users/graphql/users.entity';
+
+@ObjectType()
+export class Session {
+  @ApiProperty({ type: User })
+  @Field(() => User)
+  user: IUser;
+
+  @ApiProperty()
+  @Field()
+  token: string;
+}
